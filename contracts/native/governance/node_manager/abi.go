23f13b7d14049ad039fbc86277f54eadd9122c78
Stefan Liu <stefanliu@outlook.com>
2022-07-19 14:55:13 +0800

    Update according to upstream

--- contracts/native/governance/node_manager/abi.go
+++ contracts/native/governance/node_manager/abi.go
@@ -30,6 +30,84 @@ import (
 
 const contractName = "node manager"
 
+var (
+	MethodCancelValidator = "cancelValidator"
+
+	MethodChangeEpoch = "changeEpoch"
+
+	MethodCreateValidator = "createValidator"
+
+	MethodEndBlock = "endBlock"
+
+	MethodStake = "stake"
+
+	MethodUnStake = "unStake"
+
+	MethodUpdateCommission = "updateCommission"
+
+	MethodUpdateValidator = "updateValidator"
+
+	MethodWithdraw = "withdraw"
+
+	MethodWithdrawCommission = "withdrawCommission"
+
+	MethodWithdrawStakeRewards = "withdrawStakeRewards"
+
+	MethodWithdrawValidator = "withdrawValidator"
+
+	MethodGetAccumulatedCommission = "getAccumulatedCommission"
+
+	MethodGetAllValidators = "getAllValidators"
+
+	MethodGetCommunityInfo = "getCommunityInfo"
+
+	MethodGetCurrentEpochInfo = "getCurrentEpochInfo"
+
+	MethodGetEpochInfo = "getEpochInfo"
+
+	MethodGetGlobalConfig = "getGlobalConfig"
+
+	MethodGetOutstandingRewards = "getOutstandingRewards"
+
+	MethodGetStakeInfo = "getStakeInfo"
+
+	MethodGetStakeStartingInfo = "getStakeStartingInfo"
+
+	MethodGetTotalPool = "getTotalPool"
+
+	MethodGetUnlockingInfo = "getUnlockingInfo"
+
+	MethodGetValidator = "getValidator"
+
+	MethodGetValidatorAccumulatedRewards = "getValidatorAccumulatedRewards"
+
+	MethodGetValidatorOutstandingRewards = "getValidatorOutstandingRewards"
+
+	MethodGetValidatorSnapshotRewards = "getValidatorSnapshotRewards"
+
+	EventCancelValidator = "CancelValidator"
+
+	EventChangeEpoch = "ChangeEpoch"
+
+	EventCreateValidator = "CreateValidator"
+
+	EventStake = "Stake"
+
+	EventUnStake = "UnStake"
+
+	EventUpdateCommission = "UpdateCommission"
+
+	EventUpdateValidator = "UpdateValidator"
+
+	EventWithdraw = "Withdraw"
+
+	EventWithdrawCommission = "WithdrawCommission"
+
+	EventWithdrawStakeRewards = "WithdrawStakeRewards"
+
+	EventWithdrawValidator = "WithdrawValidator"
+)
+
 func InitABI() {
 	ab, err := abi.JSON(strings.NewReader(node_manager_abi.INodeManagerABI))
 	if err != nil {
@@ -49,11 +127,6 @@ func (m *MethodContractNameInput) Decode(payload []byte) error { return nil }
 
 type MethodContractNameOutput struct {
 	Name string
-}
-
-type MethodProposeInput struct {
-	StartHeight uint64
-	Peers       *Peers
 }
 
 type MethodProposeOutput struct {
