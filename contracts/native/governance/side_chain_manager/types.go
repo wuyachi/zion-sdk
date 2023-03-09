1a7b5599dd8b7dd0c4014322d44e1b31d940bea9
wuyachi <wuyachi@163.com>
2023-03-09 17:48:04 +0800

    Update Ripple types

--- contracts/native/governance/side_chain_manager/types.go
+++ contracts/native/governance/side_chain_manager/types.go
@@ -1,7 +1,10 @@
 package side_chain_manager
 
 import (
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
 	"math/big"
 )
 
@@ -12,4 +15,31 @@ type RippleExtraInfo struct {
 	SignerNum     uint64
 	Pks           [][]byte
 	ReserveAmount *big.Int
+}
+
+type Fee struct {
+	View uint64
+	Fee  *big.Int
+}
+
+type UpdateFeeParam struct {
+	ChainID   uint64
+	ViewNum   uint64
+	Fee       *big.Int
+	Signature []byte
+}
+
+//Digest Digest calculate the hash of param input
+func (m *UpdateFeeParam) Digest() ([]byte, error) {
+	input := &UpdateFeeParam{
+		ChainID: m.ChainID,
+		ViewNum: m.ViewNum,
+		Fee:     m.Fee,
+	}
+	msg, err := rlp.EncodeToBytes(input)
+	if err != nil {
+		return nil, fmt.Errorf("UpdateFeeParam, serialize input error: %v", err)
+	}
+	digest := crypto.Keccak256(msg)
+	return digest, nil
 }
